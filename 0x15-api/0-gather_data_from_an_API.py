14b9b9fb3b57342ec04446c059082f0683ba9c2e
aloicerains <aloiceokoth98@gmail.com>
2022-01-31 16:29:34 +0300

    pep8 updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     import requests
     import sys
 
-
     u_id = sys.argv[1]
     u_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
     api_url = "https://jsonplaceholder.typicode.com/todos/"
@@ -15,14 +14,14 @@ if __name__ == "__main__":
     task_done = 0
     total = 0
     task_names = []
-
-    for task in  tasks:
+    for task in tasks:
         user_ID = task.get("userId")
         if user_ID == int(u_id):
             total += 1
             if task.get("completed") is True:
                 task_done += 1
                 task_names.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(user_name, task_done, total))
+    print("Employee {} is done with tasks({}/{}):".format(user_name,
+          task_done, total))
     for n in task_names:
         print("\t {}".format(n))
