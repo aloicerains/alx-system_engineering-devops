11e8b6f19f42243556465b281905f80488c5dfda
aloicerains <aloiceokoth98@gmail.com>
2022-02-15 18:06:24 +0300

    edited

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,10 +18,10 @@ def number_of_subscribers(subreddit):
     if req.status_code == 200:
         url = "https://reddit.com/r/{}/about.json".format(subreddit)
         subscribers = requests.get(url, headers=headers, allow_redirects=False)
-        if subscribers.json() is None:
+        """if subscribers.json() is None:
             return (0)
         if subscribers.json().get('data') is None:
-            return (0)
+            return (0) """
         data = subscribers.json().get('data').get('subscribers')
         return (data)
     else:
