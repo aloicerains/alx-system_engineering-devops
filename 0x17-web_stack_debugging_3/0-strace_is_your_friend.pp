643395be8a96f37f952ddb3ddd1adda9d30b3412
aloicerains <aloiceokoth98@gmail.com>
2022-02-16 21:48:01 +0300

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixing apache 500 error
+exec { "fix":
+onlyif => 'test 'e /var/www/html/wp-settings.php',
+command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+path    => '/usr/local/sbin/:/usr/local/bin:/usr/sbin/:/usr/bin:/sbin/:/bin',
+}
